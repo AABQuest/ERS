6975848261a506bb3de64c7861fe7a8c31b35324
anwu123 <39736795+anwu123@users.noreply.github.com>
2018-05-29 10:59:37 -0400

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -29,7 +29,7 @@ public class Deck {
      * @param suits is an array containing all of the card suits.
      * @param values is an array containing all of the card point values.
      */
-    public Deck(String[] ranks, String[] suits, int[] values) {
+    public Deck(String[] ranks, String[] suits) {
         cards=new ArrayList<Card>();
        for (int i=0; i<ranks.length; i++){
            for (int j=0; j<suits.length; j++){
