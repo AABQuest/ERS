da57d50f19d9085a43d678cfc2748e3b9bb018d1
austintian7 <39736812+austintian7@users.noreply.github.com>
2018-05-29 10:53:11 -0400

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,18 @@
+public class Player
+{
+    private Deck d;
+    private int num;
+    
+    public Player(Deck d1, int n)
+    {
+        d = d1;
+        num = n;
+    }
+    
+    public int slap()
+    {
+        return num;
+    }
+    
+    
+}
