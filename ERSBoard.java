60990e160de5b10bd1553b6c4bdb681c68f3241a
brianyyang <39736810+brianyyang@users.noreply.github.com>
2018-05-29 11:06:26 -0400

    Add files via upload

---
+++ ERSBoard.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+/**
+ * Write a description of class ERSBoard here.
+ * Contains an x number of Player classes and an array list of Card classes
+ * Contains a method that determines if a slap is legal or illegal
+ * Contains an array that holds the order of slaps
+ * @author Brian Yang
+ */
+public class ERSBoard
+{
+    private ArrayList<Card> board;
+    private ArrayList<Integer> slapOrder;
+    private ArrayList<Player> players;
+    private Deck deck;
+    private ArrayList<Deck> starters;
+
+    /**
+     * Constructor for objects of class ERSBoard
+     */
+    public ERSBoard(int numPlayers)
+    {
+        board = new ArrayList<Card>();
+        slapOrder = new ArrayList<Integer>();
+        players = new ArrayList<Player>();
+        deck = new Deck();
+        
+        for (int x = 0; x < numPlayers; x++) {
+            players.add(new Player());
+        }
+    }
+}
+
