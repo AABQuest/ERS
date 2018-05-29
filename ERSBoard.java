9605d7a399955cf51ac7723add7b7b529f687333
anwu123 <39736795+anwu123@users.noreply.github.com>
2018-05-29 11:42:33 -0400

    Update ERSBoard.java

--- ERSBoard.java
+++ ERSBoard.java
@@ -36,14 +36,16 @@ public class ERSBoard
         return false;
     }
     public void resolution(){
-        Player first= players.get(slapOrder.get(0));
+        Player first = players.get(slapOrder.get(0));
         if(this.isLegal(board.get(board.size()-3), board.get(board.size()-2), board.get(board.size()-1)){
             for (int i = 0; i < board.size(); i++){
                 first.getDeck().add(board.get(i));
           }
         }
         else{
-            board.add(0, first.getDeck().remove(first.getDeck().size()-1));
+            for(int k = 0; k < slapOrder.size() ; k++){
+            Player slapPlayerDeck = players.get(slapOrder.get(i)).getDeck()
+            board.add(0, slapPlayerDeck.remove(slapPlayerDeck.size()-1));
         }
     }
               
