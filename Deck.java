9d4edc8d6487dc543d502f3f94630899e1d3efb9
austintian7 <39736812+austintian7@users.noreply.github.com>
2018-06-03 16:18:18 -0400

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -44,11 +44,6 @@ public class Deck {
             }
         }
     }
-    public Deck()
-    {
-        cards = new ArrayList<Cards>();
-        size = 0;
-    }
 
     /**
      * Determines if this deck is empty (no undealt cards).
