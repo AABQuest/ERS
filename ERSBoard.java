d70732422e6350b5eaf140b86778b4d5497d3e8d
austintian7 <39736812+austintian7@users.noreply.github.com>
2018-06-03 16:27:45 -0400

    Update ERSBoard.java

--- ERSBoard.java
+++ ERSBoard.java
@@ -56,15 +56,20 @@ public class ERSBoard
     }
     public void resolution(){
         Player first = players.get(slapOrder.get(0));
-        if(this.isLegal(board.get(board.size()-3), board.get(board.size()-2), board.get(board.size()-1)){
-            for (int i = 0; i < board.size(); i++){
-                first.getDeck().addCard(board.get(i));
+        if(this.isLegal(board.get(board.size()-3), board.get(board.size()-2), board.get(board.size()-1)))
+        {
+          for (int i = 0; i < board.size(); i++)
+          {
+              first.getDeck().addCard(board.get(i));
           }
         }
-        else{
-            for(int k = 0; k < slapOrder.size() ; k++){
-            Player slapPlayerDeck = players.get(slapOrder.get(i)).getDeck()
-            board.add(0, slapPlayerDeck.getDeck.deal(slapPlayerDeck.size()-1));
+        else
+        {
+            for(int k = 0; k < slapOrder.size() ; k++)
+            {
+               Deck slapPlayerDeck = players.get(slapOrder.get(k)).getDeck();
+               board.add(0, slapPlayerDeck.deal());
+            }
         }
     }
               
