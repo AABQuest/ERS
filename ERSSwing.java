d6ee53f3f47f624100062d83bb83d65e42f80505
brianyyang <39736810+brianyyang@users.noreply.github.com>
2018-06-03 17:16:32 -0400

    Update ERSSwing.java

--- ERSSwing.java
+++ ERSSwing.java
@@ -1,5 +1,3 @@
-package aabERS;
-
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,13 +7,27 @@ public class ERSSwing {
         //Create and set up the window.
         JFrame frame = new JFrame("Egyptian Ratscrew");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new CardLayout());
+        
+        //Add Menu bar
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menuFile = new JMenu("File");
+        JMenuItem menuItemExit = new JMenuItem("Exit");
+        menuFile.add(menuItemExit);
+        menuBar.setSize(1000, 100);
+        menuFile.setSize(500,100);
+         
+        menuBar.add(menuFile);
+         
+        // adds menu bar to the frame
+        frame.setJMenuBar(menuBar);
 
         //Display the window.
         frame.pack();
         frame.setSize(1000, 1000);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        frame.getContentPane().setBackground(Color.blue);
+        //frame.getContentPane().setBackground(Color.blue);
 	}
 	public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
