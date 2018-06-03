c9144705e5873c0b469ab5143bbce722afaed426
anwu123 <39736795+anwu123@users.noreply.github.com>
2018-06-03 16:17:33 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,5 +1,19 @@
-public class Player
+public class Player extends JFrame
 {
+    private static final int WF = JComponent.WHEN_FOCUSED;
+    private static final String SLAP = "slap";
+    private static final String DEAL_CARD = "deal card";
+    
+    static JLabel obj1 = new JLabel();
+    static JLabel obj2 = new JLabel();
+    
+    public Player(){
+        obj1.getInputMap(WF).put(KeyStroke.getKeyStroke("SLAP"), SLAP);
+        
+        obj2.getInputMap(WF).put(KeyStroke.getKeyStroke("W"), SLAP);
+        
+        obj1.getActionMap().put(MOVE_UP, new MoveAction(1, 1));
+    
     private Deck d;
     private int num;
     
