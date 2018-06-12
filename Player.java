a0e5eb66222c03dbb6064aca89b4f6dfedd969da
brianyyang <39736810+brianyyang@users.noreply.github.com>
2018-06-12 18:32:44 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,23 +1,57 @@
-public class Player
-{
+package aabERS;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class Player {
+	
+	private double x;
+	private double y;
     private Deck d;
     private int num;
     
-    public Player(Deck d1, int n)
-    {
+    private BufferedImage player;
+    
+    public Player(Deck d1, int n, int r, int c, double x, double y, Game game) {
         d = d1;
         num = n;
+        this.x = x;
+        this.y = y;
+        
+        SpriteSheet ss = new SpriteSheet(game.getSpriteSheet());
+        
+        player = ss.grabImage(r, c, 128, 128);
     }
+    
+    
+    public void tick() {
+    	
+    }
+    
+    public void render(Graphics g) {
+    	g.drawImage(player, (int)x, (int)y, null);
+    }
+    
     
     public int slap()
     {
         return num;
     }
     
-    public Deck getDeck()
-    {
-        return d;
+    public double getX() {
+    	return x;
     }
     
+    public double getY() {
+    	return y;
+    }
+    
+    public void setX(double x) {
+    	this.x = x;
+    }
+    
+    public void setY(double y) {
+    	this.y = y;
+    }
     
 }
